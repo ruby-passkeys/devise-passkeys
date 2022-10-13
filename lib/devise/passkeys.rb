18c5e5756b03fcbef493a82d5fe8348898301a52
Thomas Cannon <tcannon00@gmail.com>
2022-10-13 16:35:17 -0400

    `bundle gem`

---
+++ lib/devise/passkeys.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "passkeys/version"
+
+module Devise
+  module Passkeys
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/devise/passkeys/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Devise
+  module Passkeys
+    VERSION = "0.1.0"
+  end
+end
