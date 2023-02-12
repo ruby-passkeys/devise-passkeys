c4998031ec286231e76cdebee346bd23801fdbae
Thomas Cannon <tcannon00@gmail.com>
2023-02-12 12:41:54 -0500

    Test helper configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+Bundler.require(:test)
+SimpleCov.start do
+  add_filter "/test/"
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "devise/passkeys"
 
