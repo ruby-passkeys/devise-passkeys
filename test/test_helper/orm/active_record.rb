0af10a19399ea75e718524c6f5d42b193736c2fe
Thomas Cannon <tcannon00@gmail.com>
2023-02-12 13:19:46 -0500

    Fix migration path for ActiveRecord test helpers

--- test/test_helper/orm/active_record.rb
+++ test/test_helper/orm/active_record.rb
@@ -4,7 +4,7 @@ ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.logger = Logger.new(nil)
 ActiveRecord::Base.include_root_in_json = true
 
-migrate_path = File.expand_path("../../rails_app/db/migrate/", __FILE__)
+migrate_path = File.expand_path("../../../rails_app/db/migrate/", __FILE__)
 if Devise::Test.rails6_and_up?
   ActiveRecord::MigrationContext.new(migrate_path, ActiveRecord::SchemaMigration).migrate
 elsif Devise::Test.rails52_and_up?
