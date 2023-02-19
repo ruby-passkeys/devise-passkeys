45f8848dfc4252d5379a6c80096c6b0640e6d0b1
Thomas Cannon <tcannon00@gmail.com>
2023-02-19 09:46:48 -0500

    Tests for `Devise::Passkeys::Controllers::Concerns::ReauthenticationChallenge`

---
+++ test/devise/passkeys/controllers/concerns/test_reauthentication_challenge.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Devise::Passkeys::Controllers::Concerns::TestReauthenticationChallenge < ActiveSupport::TestCase
+  class TestClass
+    include Devise::Passkeys::Controllers::Concerns::ReauthenticationChallenge
+
+    attr_accessor :session
+
+    def initialize
+      self.session = {}
+    end
+
+    def resource_name
+      "test_value:1234"
+    end
+  end
+
+  setup do
+    @test_class = TestClass.new
+  end
+
+  test "#store_reauthentication_challenge_in_session" do
+    options_for_authentication = OpenStruct.new(challenge: SecureRandom.random_bytes(50))
+    assert_nil @test_class.session["test_value:1234_current_reauthentication_challenge"]
+
+    token = @test_class.store_reauthentication_challenge_in_session(options_for_authentication: options_for_authentication)
+    refute_nil token
+
+    assert_equal token, @test_class.session["test_value:1234_current_reauthentication_challenge"]
+  end
+
+  test "#passkey_reauthentication_challenge_session_key" do
+    assert_equal "test_value:1234_current_reauthentication_challenge", @test_class.passkey_reauthentication_challenge_session_key
+  end
+end
+
+class Devise::Passkeys::Controllers::Concerns::TestReauthenticationChallengeCustomization < ActiveSupport::TestCase
+  class TestClass
+    include Devise::Passkeys::Controllers::Concerns::ReauthenticationChallenge
+
+    attr_accessor :session
+
+    def initialize
+      self.session = {}
+    end
+
+    def passkey_reauthentication_challenge_session_key
+      "passkey_reauth_challenge"
+    end
+  end
+
+  setup do
+    @test_class = TestClass.new
+  end
+
+  test "#store_reauthentication_challenge_in_session" do
+    options_for_authentication = OpenStruct.new(challenge: SecureRandom.random_bytes(50))
+    assert_nil @test_class.session["passkey_reauth_challenge"]
+
+    token = @test_class.store_reauthentication_challenge_in_session(options_for_authentication: options_for_authentication)
+    refute_nil token
+
+    assert_equal token, @test_class.session["passkey_reauth_challenge"]
+  end
+
+  test "#passkey_reauthentication_challenge_session_key" do
+    assert_equal "passkey_reauth_challenge", @test_class.passkey_reauthentication_challenge_session_key
+  end
+end
