49433b4efb521791fe1d0cd5ac474f1987317688
barseek <sergey.b@hey.com>
2023-06-22 19:43:48 +0200

    Add top level & method documentation into Devise::Passkeys::Model

--- lib/devise/passkeys/model.rb
+++ lib/devise/passkeys/model.rb
@@ -2,7 +2,10 @@
 
 module Devise
   module Models
+    # This is the actual module that gets included in your
+    # model when you write `devise :passkey_authenticatable`
     module PasskeyAuthenticatable
+      # This is a callback called right after a successful passkey authentication
       def after_passkey_authentication; end
     end
   end
