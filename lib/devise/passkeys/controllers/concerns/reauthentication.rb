6d1f156f327d32f989a337ec731baa4f897c3f36
barseek <sergey.b@hey.com>
2023-06-23 12:25:22 +0200

    Add top level & method documentation for `ReauthenticationChallenge`

--- lib/devise/passkeys/controllers/concerns/reauthentication.rb
+++ lib/devise/passkeys/controllers/concerns/reauthentication.rb
@@ -9,28 +9,28 @@ module Devise
         module Reauthentication
           extend ActiveSupport::Concern
 
-          # This method is responsible for storing the reauthentication token
+          # This method is responsible for storing the reauthentication token.
           #
           # @return [String] the reauthentication token
           def store_reauthentication_token_in_session
             session[passkey_reauthentication_token_key] = Devise.friendly_token(50)
           end
 
-          # This method is responsible for retrieving the reauthentication token
+          # This method is responsible for retrieving the reauthentication token.
           #
           # @return [String] the reauthentication token
           def stored_reauthentication_token
             session[passkey_reauthentication_token_key]
           end
 
-          # This method is responsible for clearing the reauthentication token
+          # This method is responsible for clearing the reauthentication token.
           #
           # @return [String] the reauthentication token
           def clear_reauthentication_token!
             session.delete(passkey_reauthentication_token_key)
           end
 
-          # This method is responsible for consuming(i.e. retrieve & clear) the reauthentication token
+          # This method is responsible for consuming(i.e. retrieve & clear) the reauthentication token.
           #
           # @return [String] the reauthentication token
           def consume_reauthentication_token!
@@ -40,16 +40,16 @@ module Devise
           end
 
           # This method is responsible for validating the reauthentication token.
-          # Note, it will consume the stored token
+          # Note, it will consume the stored token.
           #
           # @param [String] given_reauthentication_token token to compare store token against
           def valid_reauthentication_token?(given_reauthentication_token:)
             Devise.secure_compare(consume_reauthentication_token!, given_reauthentication_token)
           end
 
-          # This method is responsible for generating the reauthentication token key
+          # This method is responsible for generating the reauthentication token session key.
           #
-          # @return [String] the reauthentication token key
+          # @return [String] the reauthentication token session key
           def passkey_reauthentication_token_key
             "#{resource_name}_current_reauthentication_token"
           end
--- lib/devise/passkeys/controllers/concerns/reauthentication_challenge.rb
+++ lib/devise/passkeys/controllers/concerns/reauthentication_challenge.rb
@@ -4,13 +4,21 @@ module Devise
   module Passkeys
     module Controllers
       module Concerns
+        # This concern is responsible for storing the reauthentication challenge in the session.
         module ReauthenticationChallenge
           extend ActiveSupport::Concern
 
+          # This method is responsible for generating the reauthentication challenge session key.
+          #
+          # @return [String] the reauthentication challenge session key
           def passkey_reauthentication_challenge_session_key
             "#{resource_name}_current_reauthentication_challenge"
           end
 
+          # This method is responsible for storing the reauthentication challenge in the session.
+          #
+          # @param [WebAuthn::PublicKeyCredential::Options] options_for_authentication the options for authentication
+          # @return [String] the reauthentication challenge
           def store_reauthentication_challenge_in_session(options_for_authentication:)
             session[passkey_reauthentication_challenge_session_key] = options_for_authentication.challenge
           end
