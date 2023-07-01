64c3cc34c40007f5ce878486e45b35b16d36fe16
Thomas Cannon <tcannon00@gmail.com>
2023-07-01 11:45:40 -0400

    Editing pass for `Devise::Passkeys` top-level module
    * Largely a signpoint, pointing to other resources, the README for
    	installation instructions
    * Also documented `create_and_return_passkey`

--- lib/devise/passkeys.rb
+++ lib/devise/passkeys.rb
@@ -11,15 +11,29 @@ require_relative "passkeys/reauthentication_strategy"
 require_relative "passkeys/version"
 
 module Devise
-  # Devise extension to use passkeys instead of passwords
-  #   Passkeys are the new standard to authenticate on the web: a safer and easier replacement for passwords
-  #   From a technical point of view, passkeys are a public/private keypair, created by a personal device
-  #     for an specific app or site (referenced as Relying Party - RP), using either a device biometrics or
-  #     a external security key
-  #   The private key is kept in the device and stored in the cloud, while the public key is sent to the
-  #   app/site that it was created for
-  #   All sub-modules, models, classes, concerns etc are inherited from here
+  # This module provides a devise extension to use passkeys instead
+  # of passwords for user authentication.
+  #
+  # It is lightweight and non-configurable. It does what it has to do and
+  # leaves some manual implementation to you.
+  #
+  # Please consult the {file:README.md#label-Usage} for installation & configuration instructions;
+  # and the links below for additional reading about:
+  #
+  # - What passkeys are
+  # - The underlying gems used to build this devise extension
+  # - Platform support & user interface implementation guides
+  #
+  # @see https://webauthn.guide
+  # @see https://passkeys.dev
+  # @see https://fidoalliance.org/passkeys
+  # @see https://github.com/cedarcode/webauthn-ruby
+  # @see https://github.com/ruby-passkeys/warden-webauthn
   module Passkeys
+    # This is a helper method that creates and returns a passkey for
+    # the given User (`resource`), using the provided label & `WebAuthn::Credential`
+    # @see PasskeyIssuer#create_and_return_passkey
+    # @return A saved passkey for the the given User (`resource`)
     def self.create_and_return_passkey(resource:, label:, webauthn_credential:, extra_attributes: {})
       PasskeyIssuer.build.create_and_return_passkey(
         resource: resource,
