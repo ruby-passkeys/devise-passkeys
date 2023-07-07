e2a1db19ae3c7a255a0533a70e88551c7a91e0a2
Thomas Cannon <tcannon00@gmail.com>
2023-07-07 09:29:50 -0400

    Editing for `Devise::Passkeys::PasskeyAuthenticatable` README references
    * Wrap in a `file:` tag that YARD can use, add formatting for note about
    	README steps being required.

--- lib/devise/passkeys/model.rb
+++ lib/devise/passkeys/model.rb
@@ -6,12 +6,12 @@ module Devise
     # model when you include `:passkey_authenticatable` in the
     # `devise` call (eg: `devise :passkey_authenticatable, ...`).
     #
-    # For more thorough steps on setting up the model please follow link below.
-    # @see README 
+    # Please consult the {file:README.md#label-Usage} for model installation
+    # & configuration instructions.
     module PasskeyAuthenticatable
       # This is a callback that is called right after a successful passkey authentication.
       #
-      # *Important*: Must follow the steps in the README documentation to fully set up a model
+      # **Important**: You **must** follow the steps in the {file:README.md#label-Usage} to fully set up a model
       # and use passkeys.  
       #
       # By default, it is a no-op, but you can override it in your model for any custom behavior
